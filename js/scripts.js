7d402d3fdf28bc1f4045427a81d0206c432886f1
DanielPuentes10 <95193867+DanielPuentes10@users.noreply.github.com>
2022-06-12 21:42:20 -0500

    segundo commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function menu(){
+
+
+let menu =document.querySelector('#menu');
+
+let menu_bar = document.querySelector('#menu-bar');
+
+    if(menu.className == 'menu'){
+       
+        menu.className = 'menuac';
+        
+    }else{
+       menu.className = 'menu';
+       
+    }
+    
+}
+
+
